9046ff5d9027ad31cd6f441eda4d3d4e583be8ac
AchuSunil <94796358+AchuSunil@users.noreply.github.com>
2021-11-29 15:19:02 +0530

    Add files via upload

--- new-script.js
+++ new-script.js
@@ -1,10 +1,11 @@
 var username = document.getElementById("username")
 var emaildata =document.getElementById("emailadd")
 var numberdata =document.getElementById("phonenum")
+var messagedata=document.getElementById("msg")
 var err = document.getElementsByClassName("wrong")
 var usrnameRegex = /^[A-Za-z]+$/
 var emladdRegex= /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var phnumRegex = /[0-9]/g
+var phnumRegex = /^\d+$/;
 var achu = 0
 
 $("#submit-form").submit((e)=>{
@@ -14,9 +15,10 @@ $("#submit-form").submit((e)=>{
     checkName()
     checkNumber()
     checkEmail()
+    checkmessage()
 
 
-    if(achu == 3){
+    if(achu == 4){
         $.ajax({
             url:"https://script.google.com/macros/s/AKfycbxGb9DPr8wYhYlXC7NzSwQSO1ftGEmZ1J8zetmqtAikJbDvU-g/exec?",
             data:$("#submit-form").serialize(),
@@ -36,11 +38,11 @@ $("#submit-form").submit((e)=>{
 
 function checkName(){
     if(username.value == ""){
-         err[0].innerHTML = "fill this field"
+         err[0].innerHTML = "*Fill this Field"
     }else if(username.value.trim() == ""){
-        err[0].innerHTML = "space not allowed"
+        err[0].innerHTML = "*Space not allowed"
     }else if(usrnameRegex.test(username.value) == false){
-        err[0].innerHTML = "only character allowed"
+        err[0].innerHTML = "*Only character allowed"
     }else{
         err[0].innerHTML = ""
         achu += 1
@@ -51,16 +53,16 @@ function checkNumber() {
 
 
     if (numberdata.value == "") {
-        err[1].innerHTML="Enter Number"
+        err[1].innerHTML="*Enter Number"
 
     } else if (numberdata.value.trim() == false) {
-        err[1].innerHTML="Space Not Allowed"
+        err[1].innerHTML="*Space Not Allowed"
 
     } else if (phnumRegex.test(numberdata.value) == false) {
-        err[1].innerHTML="Enter Numbers"
+        err[1].innerHTML="*Enter Numbers"
 
     }else if(numberdata.value.length >10 || numberdata.value.length<10){
-        err[1].innerHTML="Enter 10 Digits"
+        err[1].innerHTML="*Enter 10 Digits"
     }
      else {
         err[1].innerHTML = ""
@@ -70,16 +72,25 @@ function checkNumber() {
 function checkEmail() {
 
     if (emaildata.value == "") {
-        err[2].innerHTML="Enter Email"     
+        err[2].innerHTML="*Enter Email"     
 
     } else if (emaildata.value.trim() == false) {
-        err[2] .innerHTML="Space Not Allowed"
+        err[2] .innerHTML="*Space Not Allowed"
 
     } else if (emladdRegex.test(emaildata.value) == false) {
-        err[2] .innerHTML="Input Valid Email Address"
+        err[2] .innerHTML="*Input Valid Email Address"
 
     } else {
         err[2].innerHTML = ""
         achu += 1
     }
+}
+function checkMsg(){
+    if(messagedata.value.trim()==false){
+        err[3] .innerHTML="*Space Not Allowed"
+    }else{
+        err[3].innerHTML = ""
+        achu += 1 
+    }
+
 }
